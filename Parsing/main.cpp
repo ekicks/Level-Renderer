aaa245807793913014601ffdec1568eb5a07f26a
Ethan Ortega <ekicks0@gmail.com>
2022-10-05 19:36:20 -0400

    pushing parsing level data

---
+++ Parsing/main.cpp
@@ -0,0 +1,132 @@
+// Simple basecode showing how to create a window and attatch a d3d11surface
+#define GATEWARE_ENABLE_CORE // All libraries need this
+#define GATEWARE_ENABLE_SYSTEM // Graphics libs require system level libraries
+#define GATEWARE_ENABLE_GRAPHICS // Enables all Graphics Libraries
+// Ignore some GRAPHICS libraries we aren't going to use
+#define GATEWARE_DISABLE_GDIRECTX12SURFACE // we have another template for this
+#define GATEWARE_DISABLE_GRASTERSURFACE // we have another template for this
+#define GATEWARE_DISABLE_GOPENGLSURFACE // we have another template for this
+#define GATEWARE_DISABLE_GVULKANSURFACE // we have another template for this
+// With what we want & what we don't defined we can include the API
+#define GATEWARE_ENABLE_INPUT
+#include "Gateware.h"
+#include "renderer.h" // example rendering code (not Gateware code!)
+#include "h2bParser.h"
+#include <string.h>
+// open some namespaces to compact the code a bit
+using namespace GW;
+using namespace CORE;
+using namespace SYSTEM;
+using namespace GRAPHICS;
+// lets pop a window and use D3D11 to clear to a green screen
+void parseFile();
+
+int main()
+{
+	parseFile();
+	GWindow win;
+	GEventResponder msgs;
+	GDirectX11Surface d3d11;
+	if (+win.Create(0, 0, 800, 600, GWindowStyle::WINDOWEDBORDERED))
+	{
+		float clr[] = { 57/255.0f, 1.0f, 20/255.0f, 1 }; // start with a neon green
+		msgs.Create([&](const GW::GEvent& e) {
+			GW::SYSTEM::GWindow::Events q;
+			if (+e.Read(q) && q == GWindow::Events::RESIZE)
+				clr[2] += 0.01f; // move towards a cyan as they resize
+		});
+		win.Register(msgs);
+		if (+d3d11.Create(win, GW::GRAPHICS::DEPTH_BUFFER_SUPPORT))
+		{
+			Renderer renderer(win, d3d11);
+			while (+win.ProcessWindowEvents())
+			{
+				IDXGISwapChain* swap;
+				ID3D11DeviceContext* con;
+				ID3D11RenderTargetView* view;
+				ID3D11DepthStencilView* depth;
+				if (+d3d11.GetImmediateContext((void**)&con) &&
+					+d3d11.GetRenderTargetView((void**)&view) &&
+					+d3d11.GetDepthStencilView((void**)&depth) &&
+					+d3d11.GetSwapchain((void**)&swap))
+				{
+					con->ClearRenderTargetView(view, clr);
+					con->ClearDepthStencilView(depth, D3D11_CLEAR_DEPTH, 1, 0);
+					renderer.Render();
+					swap->Present(1, 0);
+					// release incremented COM reference counts
+					swap->Release();
+					view->Release();
+					depth->Release();
+					con->Release();
+				}
+			}
+		}
+	}
+	return 0; // that's all folks
+}
+
+void parseFile() {
+	std::ifstream file;
+	DirectX::XMFLOAT4X4 objMatrix;
+	float objVect[4];
+	std::vector< DirectX::XMMATRIX> matrixVect;
+	std::string valueString;
+	std::string lastCharacter;
+	std::string character;
+	float value;
+	int count =0;
+	int count2 =0;
+
+	file.open("../GameLevel.txt");
+	while (true) {
+		std::string output = "";
+		H2B::Parser parser;
+		std::string h2b = "";
+		std::string matrix = "";
+		
+
+		if (file.eof() == true) {
+			break;
+		}
+
+		std::getline(file, output, '\n');
+
+		if (std::strcmp(output.c_str(), "MESH") == 0 || std::strcmp(output.c_str(), "LIGHT") == 0 || std::strcmp(output.c_str(), "CAMERA") == 0) {
+			std::cout << output << std::endl;
+			std::getline(file, h2b, '\n');
+			std::cout << h2b << std::endl;
+			std::getline(file, matrix, '(');
+			std::cout << matrix << "(";
+			std::getline(file, matrix, '>');
+			std::cout << matrix << ">" << std::endl;
+			for (int i = 0; i < matrix.length(); i++)
+			{
+				character = matrix[i];
+				if ((character[0] >= 48 && character[0] <= 57 || character[0] == 46 || character[0] == 45)) {
+					valueString.append(character);
+				}
+				if (matrix[i] == ',' || matrix[i] == ')') {
+
+					value = std::stof(valueString);
+					objVect[count] = value;
+					count++;
+					if (count == 4) {
+						count = 0;
+						objMatrix.m[count2][0] = objVect[0];
+						objMatrix.m[count2][1] = objVect[1];
+						objMatrix.m[count2][2] = objVect[2];
+						objMatrix.m[count2][3] = objVect[3];
+						count2++;
+						if (count2 == 4) {
+							count2 = 0;
+						}
+					}
+					valueString = "";
+				}
+			}
+		}
+		
+	}
+	file.close();
+}
---
+++ Parsing/renderer.h
@@ -0,0 +1,322 @@
+// minimalistic code to draw a single triangle, this is not part of the API.
+// required for compiling shaders on the fly, consider pre-compiling instead
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#pragma comment(lib, "d3dcompiler.lib")
+// Simple Vertex Shader
+const char* vertexShaderSource = R"(
+// an ultra simple hlsl vertex shader
+cbuffer ConstantBuffer
+{
+	matrix matrixOne;
+	matrix matrixTwo;
+    matrix matrixThree;
+};
+float4 main(float4 inputVertex : POSITION) : SV_POSITION
+{
+	float4 worldPos = mul(matrixOne, inputVertex);
+    float4 viewPos = mul(matrixTwo, worldPos);
+    float4 perspective = mul(matrixThree, viewPos);
+	return perspective;
+}
+)";
+// Simple Pixel Shader
+const char* pixelShaderSource = R"(
+// an ultra simple hlsl pixel shader
+float4 main() : SV_TARGET 
+{	
+	return float4(0.25f,0.0f,1.0f,0); 
+}
+)";
+struct Vertex
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+
+struct ConstantBuffer
+{
+	DirectX::XMMATRIX matrixOne;
+	DirectX::XMMATRIX matrixTwo;
+	DirectX::XMMATRIX matrixThree;
+};
+
+using namespace std::chrono;
+// Creation, Rendering & Cleanup
+class Renderer
+{
+	// proxy handles
+	GW::SYSTEM::GWindow win;
+	GW::GRAPHICS::GDirectX11Surface d3d;
+	// what we need at a minimum to draw a triangle
+	Microsoft::WRL::ComPtr<ID3D11Buffer>		vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer>		constantBuffer;
+	Microsoft::WRL::ComPtr<ID3D11VertexShader>	vertexShader;
+	Microsoft::WRL::ComPtr<ID3D11PixelShader>	pixelShader;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout>	vertexFormat;
+
+	GW::INPUT::GInput input;
+	DirectX::XMMATRIX viewMatrix;
+	high_resolution_clock::time_point startTime = high_resolution_clock::now();
+	
+public:
+	Renderer(GW::SYSTEM::GWindow _win, GW::GRAPHICS::GDirectX11Surface _d3d)
+	{
+		win = _win;
+		d3d = _d3d;
+		ID3D11Device* creator;
+		d3d.GetDevice((void**)&creator);
+
+		input.Create(_win);
+
+		DirectX::FXMVECTOR eye{ 0.25f, -0.125f, -0.25f };
+		DirectX::FXMVECTOR focus{ 0,-0.5,0 };
+		DirectX::FXMVECTOR upDir{ 0,1,0 };
+
+		viewMatrix = DirectX::XMMatrixLookAtLH(eye, focus, upDir);
+
+		// Create Vertex Buffer
+		Vertex verts[104] = {
+			 0
+		};
+		float yValue = -0.5;
+		float xValue = -0.5;
+		float value = 0;
+		float value2 = 0;
+		for (int i = 0; i < 104;)
+		{
+			if (i >= 52) {
+				verts[i] = {(xValue + (float)(value2 / 25.0f)), -0.5, 0, 1 };
+				verts[i + 1] = {(xValue + (float)(value2 / 25.0f)), 0.5, 0, 1 };
+				i += 2;
+				value2++;
+			}
+			else {
+				verts[i] = { -0.5,(yValue + (float)(value / 25.0f)),0,1 };
+				verts[i + 1] = { 0.5,(yValue + (float)(value / 25.0f)),0,1 };
+				i += 2;
+				value++;
+			}
+		}
+
+		CD3D11_BUFFER_DESC cDesc(sizeof(ConstantBuffer), D3D11_BIND_CONSTANT_BUFFER);
+		creator->CreateBuffer(&cDesc, nullptr, constantBuffer.GetAddressOf());
+		
+		D3D11_SUBRESOURCE_DATA bData = { verts, 0, 0 };
+		CD3D11_BUFFER_DESC bDesc(sizeof(verts), D3D11_BIND_VERTEX_BUFFER);
+		creator->CreateBuffer(&bDesc, &bData, vertexBuffer.GetAddressOf());
+
+
+		// Create Vertex Shader
+		UINT compilerFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if _DEBUG
+		compilerFlags |= D3DCOMPILE_DEBUG;
+#endif
+		Microsoft::WRL::ComPtr<ID3DBlob> vsBlob, errors;
+		if (SUCCEEDED(D3DCompile(vertexShaderSource, strlen(vertexShaderSource),
+			nullptr, nullptr, nullptr, "main", "vs_4_0", compilerFlags, 0, 
+			vsBlob.GetAddressOf(), errors.GetAddressOf())))
+		{
+			creator->CreateVertexShader(vsBlob->GetBufferPointer(),
+				vsBlob->GetBufferSize(), nullptr, vertexShader.GetAddressOf());
+		}
+		else
+			std::cout << (char*)errors->GetBufferPointer() << std::endl;
+		// Create Pixel Shader
+		Microsoft::WRL::ComPtr<ID3DBlob> psBlob; errors.Reset();
+		if (SUCCEEDED(D3DCompile(pixelShaderSource, strlen(pixelShaderSource),
+			nullptr, nullptr, nullptr, "main", "ps_4_0", compilerFlags, 0, 
+			psBlob.GetAddressOf(), errors.GetAddressOf())))
+		{
+			creator->CreatePixelShader(psBlob->GetBufferPointer(),
+				psBlob->GetBufferSize(), nullptr, pixelShader.GetAddressOf());
+		}
+		else
+			std::cout << (char*)errors->GetBufferPointer() << std::endl;
+		// Create Input Layout
+		D3D11_INPUT_ELEMENT_DESC format[] = {
+			{ 
+				"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 
+				D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 
+			}
+		};
+		creator->CreateInputLayout(format, ARRAYSIZE(format), 
+			vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), 
+			vertexFormat.GetAddressOf());
+		// free temporary handle
+		creator->Release();
+	}
+	void Render()
+	{
+		DirectX::XMMATRIX m;
+		DirectX::XMMATRIX m2;
+		DirectX::XMMATRIX m3;
+		DirectX::XMMATRIX m4;
+		DirectX::XMMATRIX m5;
+		DirectX::XMMATRIX m6;
+	
+		m = DirectX::XMMatrixIdentity();
+		m = DirectX::XMMatrixMultiply(m, DirectX::XMMatrixRotationX(1.5708f));
+		m = DirectX::XMMatrixMultiply(m, DirectX::XMMatrixTranslation(0.0f, 0.5f, 0.0f));
+
+
+		float wKey, aKey, sKey, dKey, space, shift;
+		float mouseX, mouseY;
+		input.GetState(G_KEY_W, wKey);
+		input.GetState(G_KEY_A, aKey);
+		input.GetState(G_KEY_S, sKey);
+		input.GetState(G_KEY_D, dKey);
+		input.GetState(G_KEY_LEFTSHIFT, shift);
+		input.GetState(G_KEY_SPACE, space);
+		GW::GReturn result = input.GetMouseDelta(mouseX, mouseY);
+		high_resolution_clock::time_point currTime = high_resolution_clock::now();
+		duration<double> timespan = duration_cast<duration<double>>(currTime - startTime);
+		if (timespan.count() > 0.0167) {
+			if (result == GW::GReturn::SUCCESS) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixRotationY(mouseX / 360.0f), viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixRotationX(mouseY / 360.0f), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			else {
+				mouseX = 0;
+				mouseY = 0;
+			}
+			if (wKey > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(0, 0, 0.005f), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			if (aKey > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(-0.005f, 0, 0),viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			if (sKey > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(0, 0, -0.005f), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			if (dKey > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply( DirectX::XMMatrixTranslation(0.005f, 0, 0), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			if (shift > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(0, -0.005f, 0), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			if (space > 0) {
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+				viewMatrix = DirectX::XMMatrixMultiply(DirectX::XMMatrixTranslation(0, 0.005f, 0), viewMatrix);
+				viewMatrix = DirectX::XMMatrixInverse(nullptr, viewMatrix);
+			}
+			startTime = high_resolution_clock::now();
+		}
+
+
+		float aRatio;
+		d3d.GetAspectRatio(aRatio);
+
+		DirectX::XMMATRIX perspectiveMatrix = DirectX::XMMatrixPerspectiveFovLH(1.13446f, aRatio, 0.1f, 100.0f);
+
+		ConstantBuffer cbuffer = { m, viewMatrix , perspectiveMatrix};
+
+		m2 = DirectX::XMMatrixIdentity();
+		m2 = DirectX::XMMatrixMultiply(m2, DirectX::XMMatrixRotationX(-1.5708f));
+		m2 = DirectX::XMMatrixMultiply(m2, DirectX::XMMatrixTranslation(0.0f, -0.5f, 0.0f));
+
+		m3 = DirectX::XMMatrixIdentity();
+		m3 = DirectX::XMMatrixMultiply(m3, DirectX::XMMatrixRotationX(0));
+		m3 = DirectX::XMMatrixMultiply(m3, DirectX::XMMatrixTranslation(0.0f, 0, 0.5f));
+
+		m4 = DirectX::XMMatrixIdentity();
+		m4 = DirectX::XMMatrixMultiply(m4, DirectX::XMMatrixRotationX(3.14159f));
+		m4 = DirectX::XMMatrixMultiply(m4, DirectX::XMMatrixTranslation(0.0f, 0, -0.5f));
+
+		m5 = DirectX::XMMatrixIdentity();
+		m5 = DirectX::XMMatrixMultiply(m5, DirectX::XMMatrixRotationY(-1.5708f));
+		m5 = DirectX::XMMatrixMultiply(m5, DirectX::XMMatrixTranslation(-0.5f, 0, 0));
+
+		m6 = DirectX::XMMatrixIdentity();
+		m6 = DirectX::XMMatrixMultiply(m6, DirectX::XMMatrixRotationY(-1.5708f));
+		m6 = DirectX::XMMatrixMultiply(m6, DirectX::XMMatrixTranslation(0.5f, 0, 0));
+
+
+
+		// grab the context & render target
+		ID3D11DeviceContext* con;
+		ID3D11RenderTargetView* view;
+		ID3D11DepthStencilView* depth;
+
+		d3d.GetImmediateContext((void**)&con);
+		d3d.GetRenderTargetView((void**)&view);
+		d3d.GetDepthStencilView((void**)&depth);
+		// setup the pipeline
+		ID3D11RenderTargetView *const views[] = { view };
+		con->OMSetRenderTargets(ARRAYSIZE(views), views, depth);
+		const UINT strides[] = { sizeof(Vertex) };
+		const UINT offsets[] = { 0 };
+		ID3D11Buffer* const buffs[] = { vertexBuffer.Get() };
+		con->IASetVertexBuffers(0, ARRAYSIZE(buffs), buffs, strides, offsets);
+		con->VSSetShader(vertexShader.Get(), nullptr, 0);
+		con->PSSetShader(pixelShader.Get(), nullptr, 0);
+		con->IASetInputLayout(vertexFormat.Get());
+
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+
+		// now we can draw
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+
+		cbuffer.matrixOne = m2;
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+
+		cbuffer.matrixOne = m3;
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+		
+		cbuffer.matrixOne = m4;
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+		
+		cbuffer.matrixOne = m5;
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+		
+		cbuffer.matrixOne = m6;
+		con->UpdateSubresource(constantBuffer.Get(), 0, nullptr, &cbuffer, 0, 0);
+		con->VSSetConstantBuffers(0, 1, constantBuffer.GetAddressOf());
+
+		con->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_LINELIST);
+		con->Draw(104, 0);
+
+
+		// release temp handles
+		depth->Release();
+		view->Release();
+		con->Release();
+	}
+	~Renderer()
+	{
+		// ComPtr will auto release so nothing to do here 
+	}
+};
